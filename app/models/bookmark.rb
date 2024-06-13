1f2e03b01191ccc02533825a0302327689ac064b
alejandra gonzalez <lagonzalez.grafica@gmail.com>
2024-06-13 15:49:30 -0500

    watch list seeds

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,10 @@
 class Bookmark < ApplicationRecord
+  # belongs to a movie
+  # belongs to an list
   belongs_to :movie
   belongs_to :list
 
   validates :movie, uniqueness: { scope: :list }
+  # comment cannot be shorter than 6 characters
   validates :comment, length: { minimum: 6 }
 end
